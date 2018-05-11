741badb077255ce619057f90c3ad1332ec01b680
Vinh <vinhnguyenuit2109@gmail.com>
2018-05-11 10:31:52 +0700

    update sql + flight search

--- Airline/SearchControl.Designer.cs
+++ Airline/SearchControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Airline
+﻿﻿namespace Airline
 {
     partial class SearchControl
     {
@@ -38,17 +38,18 @@
             this.bunifuDatepicker1 = new Bunifu.Framework.UI.BunifuDatepicker();
             this.searchBt = new Bunifu.Framework.UI.BunifuFlatButton();
             this.flightInfo = new Bunifu.Framework.UI.BunifuCustomDataGrid();
-            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.from = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.to = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.yearRp = new Bunifu.Framework.UI.BunifuDropdown();
             this.bunifuDropdown1 = new Bunifu.Framework.UI.BunifuDropdown();
             this.label4 = new System.Windows.Forms.Label();
             this.bunifuDropdown2 = new Bunifu.Framework.UI.BunifuDropdown();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.from = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.to = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cl1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.flightInfo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,19 +57,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(133, 91);
+            this.label1.Location = new System.Drawing.Point(95, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "From: ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label2.Location = new System.Drawing.Point(151, 141);
+            this.label2.Location = new System.Drawing.Point(113, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 17);
             this.label2.TabIndex = 1;
@@ -79,12 +79,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label3.Location = new System.Drawing.Point(72, 195);
+            this.label3.Location = new System.Drawing.Point(34, 178);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Departure Date";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // bunifuDatepicker1
             // 
@@ -93,7 +92,7 @@
             this.bunifuDatepicker1.ForeColor = System.Drawing.Color.White;
             this.bunifuDatepicker1.Format = System.Windows.Forms.DateTimePickerFormat.Long;
             this.bunifuDatepicker1.FormatCustom = null;
-            this.bunifuDatepicker1.Location = new System.Drawing.Point(227, 195);
+            this.bunifuDatepicker1.Location = new System.Drawing.Point(189, 178);
             this.bunifuDatepicker1.Name = "bunifuDatepicker1";
             this.bunifuDatepicker1.Size = new System.Drawing.Size(255, 29);
             this.bunifuDatepicker1.TabIndex = 3;
@@ -120,7 +119,7 @@
             this.searchBt.IconVisible = true;
             this.searchBt.IconZoom = 65D;
             this.searchBt.IsTab = true;
-            this.searchBt.Location = new System.Drawing.Point(227, 305);
+            this.searchBt.Location = new System.Drawing.Point(189, 288);
             this.searchBt.Name = "searchBt";
             this.searchBt.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
             this.searchBt.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(76)))), ((int)(((byte)(157)))));
@@ -132,6 +131,7 @@
             this.searchBt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.searchBt.Textcolor = System.Drawing.Color.White;
             this.searchBt.TextFont = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBt.Click += new System.EventHandler(this.searchBt_Click);
             // 
             // flightInfo
             // 
@@ -157,9 +157,10 @@
             this.from,
             this.to,
             this.time,
-            this.price,
             this.cl1,
-            this.Column1});
+            this.price,
+            this.Column1,
+            this.price2});
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -178,39 +179,10 @@
             this.flightInfo.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.flightInfo.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.flightInfo.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
-            this.flightInfo.Size = new System.Drawing.Size(745, 155);
+            this.flightInfo.Size = new System.Drawing.Size(583, 155);
             this.flightInfo.TabIndex = 8;
             this.flightInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bunifuCustomDataGrid1_CellContentClick);
-            // 
-            // stt
-            // 
-            this.stt.HeaderText = "Flight Code";
-            this.stt.Name = "stt";
-            this.stt.ReadOnly = true;
-            // 
-            // from
-            // 
-            this.from.HeaderText = "From";
-            this.from.Name = "from";
-            this.from.ReadOnly = true;
-            // 
-            // to
-            // 
-            this.to.HeaderText = "To";
-            this.to.Name = "to";
-            this.to.ReadOnly = true;
-            // 
-            // time
-            // 
-            this.time.HeaderText = "Departure Time";
-            this.time.Name = "time";
-            this.time.ReadOnly = true;
-            // 
-            // price
-            // 
-            this.price.HeaderText = "Price";
-            this.price.Name = "price";
-            this.price.ReadOnly = true;
+            this.flightInfo.SelectionChanged += new System.EventHandler(this.flightInfo_SelectionChanged);
             // 
             // yearRp
             // 
@@ -221,9 +193,8 @@
             this.yearRp.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.yearRp.ForeColor = System.Drawing.Color.White;
             this.yearRp.Items = new string[] {
-        "HCM",
-        "HN"};
-            this.yearRp.Location = new System.Drawing.Point(227, 91);
+        "HCM"};
+            this.yearRp.Location = new System.Drawing.Point(189, 74);
             this.yearRp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.yearRp.Name = "yearRp";
             this.yearRp.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
@@ -231,7 +202,7 @@
             this.yearRp.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.yearRp.selectedIndex = 0;
             this.yearRp.Size = new System.Drawing.Size(187, 31);
-            this.yearRp.TabIndex = 9;
+            this.yearRp.TabIndex = 10;
             this.yearRp.onItemSelected += new System.EventHandler(this.yearRp_onItemSelected);
             // 
             // bunifuDropdown1
@@ -242,15 +213,14 @@
             this.bunifuDropdown1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuDropdown1.ForeColor = System.Drawing.Color.White;
             this.bunifuDropdown1.Items = new string[] {
-        "HCM",
         "HN"};
-            this.bunifuDropdown1.Location = new System.Drawing.Point(227, 141);
+            this.bunifuDropdown1.Location = new System.Drawing.Point(189, 124);
             this.bunifuDropdown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuDropdown1.Name = "bunifuDropdown1";
             this.bunifuDropdown1.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
             this.bunifuDropdown1.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
             this.bunifuDropdown1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.bunifuDropdown1.selectedIndex = 1;
+            this.bunifuDropdown1.selectedIndex = 0;
             this.bunifuDropdown1.Size = new System.Drawing.Size(187, 31);
             this.bunifuDropdown1.TabIndex = 10;
             this.bunifuDropdown1.onItemSelected += new System.EventHandler(this.bunifuDropdown1_onItemSelected);
@@ -260,7 +230,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label4.Location = new System.Drawing.Point(75, 248);
+            this.label4.Location = new System.Drawing.Point(37, 231);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 17);
             this.label4.TabIndex = 11;
@@ -274,26 +244,74 @@
             this.bunifuDropdown2.DisabledColor = System.Drawing.Color.Gray;
             this.bunifuDropdown2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuDropdown2.ForeColor = System.Drawing.Color.White;
-            this.bunifuDropdown2.Items = new string[0];
-            this.bunifuDropdown2.Location = new System.Drawing.Point(227, 248);
+            this.bunifuDropdown2.Items = new string[] {
+        "Any Time"};
+            this.bunifuDropdown2.Location = new System.Drawing.Point(189, 231);
             this.bunifuDropdown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuDropdown2.Name = "bunifuDropdown2";
             this.bunifuDropdown2.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
             this.bunifuDropdown2.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(88)))), ((int)(((byte)(173)))));
             this.bunifuDropdown2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.bunifuDropdown2.selectedIndex = -1;
+            this.bunifuDropdown2.selectedIndex = 0;
             this.bunifuDropdown2.Size = new System.Drawing.Size(187, 31);
             this.bunifuDropdown2.TabIndex = 12;
+            this.bunifuDropdown2.onItemSelected += new System.EventHandler(this.bunifuDropdown2_onItemSelected);
+            // 
+            // stt
+            // 
+            this.stt.DataPropertyName = "MACHUYENBAY";
+            this.stt.HeaderText = "Flight Code";
+            this.stt.Name = "stt";
+            this.stt.ReadOnly = true;
+            // 
+            // from
+            // 
+            this.from.DataPropertyName = "SANBAYDI";
+            this.from.HeaderText = "From";
+            this.from.Name = "from";
+            this.from.ReadOnly = true;
+            // 
+            // to
+            // 
+            this.to.DataPropertyName = "SANBAYDEN";
+            this.to.HeaderText = "To";
+            this.to.Name = "to";
+            this.to.ReadOnly = true;
+            // 
+            // time
+            // 
+            this.time.DataPropertyName = "GIO";
+            this.time.HeaderText = "Departure Time";
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
             // 
             // cl1
             // 
-            this.cl1.HeaderText = "No. 1st class tickets left";
+            this.cl1.DataPropertyName = "HANG1CONLAI";
+            this.cl1.HeaderText = "No. 1st class";
             this.cl1.Name = "cl1";
+            this.cl1.ReadOnly = true;
+            // 
+            // price
+            // 
+            this.price.DataPropertyName = "price1";
+            this.price.HeaderText = "Price 1st Class";
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
             // 
             // Column1
             // 
-            this.Column1.HeaderText = "No. 2nd class tickets left";
+            this.Column1.DataPropertyName = "HANG2CONLAI";
+            this.Column1.HeaderText = "No. 2nd class";
             this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // price2
+            // 
+            this.price2.DataPropertyName = "GIAVE";
+            this.price2.HeaderText = "Price 2nd Class";
+            this.price2.Name = "price2";
+            this.price2.ReadOnly = true;
             // 
             // SearchControl
             // 
@@ -326,16 +344,17 @@
         private Bunifu.Framework.UI.BunifuDatepicker bunifuDatepicker1;
         private Bunifu.Framework.UI.BunifuFlatButton searchBt;
         private Bunifu.Framework.UI.BunifuCustomDataGrid flightInfo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
-        private System.Windows.Forms.DataGridViewTextBoxColumn from;
-        private System.Windows.Forms.DataGridViewTextBoxColumn to;
-        private System.Windows.Forms.DataGridViewTextBoxColumn time;
-        private System.Windows.Forms.DataGridViewTextBoxColumn price;
         private Bunifu.Framework.UI.BunifuDropdown yearRp;
         private Bunifu.Framework.UI.BunifuDropdown bunifuDropdown1;
         private System.Windows.Forms.Label label4;
         private Bunifu.Framework.UI.BunifuDropdown bunifuDropdown2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn from;
+        private System.Windows.Forms.DataGridViewTextBoxColumn to;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
         private System.Windows.Forms.DataGridViewTextBoxColumn cl1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price2;
     }
 }
--- Airline/SearchControl.cs
+++ Airline/SearchControl.cs
@@ -8,48 +8,141 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
 namespace Airline
 {
     public partial class SearchControl : UserControl
     {
+
         public SearchControl()
         {
             InitializeComponent();
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void bunifuCustomDataGrid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
+            ConnectToSQL con = new ConnectToSQL();
+            con.OpenConn();
+            string sql = "SELECT MACHUYENBAY, SANBAYDI, SANBAYDEN, GIO, HANG1CONLAI, GIAVE*1.05 as price1, HANG2CONLAI, GIAVE " +
+                "FROM dbo.CHUYENBAY";
+            SqlCommand cmd = new SqlCommand(sql, con.Connection);
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.CloseConn();
+            flightInfo.DataSource = dt;
 
         }
 
         private void bunifuDropdown1_onItemSelected(object sender, EventArgs e)
         {
+            ConnectToSQL con = new ConnectToSQL();
+            con.OpenConn();
+            string sql = "SELECT MASANBAY FROM dbo.SANBAY ORDER BY MASANBAY ASC";
+            SqlCommand cmd = new SqlCommand(sql, con.Connection);
+            SqlDataReader myReader;
+            try
+            {
+                myReader = cmd.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    while (myReader.Read())
+                    {
+                        bunifuDropdown1.AddItem(myReader[0].ToString());
+                    }
+                }
+            }
+            catch { }
+            con.CloseConn();
 
         }
 
         private void yearRp_onItemSelected(object sender, EventArgs e)
         {
-
+            ConnectToSQL con = new ConnectToSQL();
+            con.OpenConn();
+            string sql = "SELECT MASANBAY FROM dbo.SANBAY ORDER BY MASANBAY ASC";
+            SqlCommand cmd = new SqlCommand(sql, con.Connection);
+            SqlDataReader myReader;
+            try
+            {
+                myReader = cmd.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    while (myReader.Read())
+                    {
+                        string sName = myReader[0].ToString();
+                        yearRp.AddItem(sName);
+                    }
+                }
+            }
+            catch { }
+            con.CloseConn();
         }
-
+       
         private void SearchControl_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void bunifuDatepicker1_onValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void flightInfo_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                flightInfo.CurrentRow.Selected = true;
+            }
+            catch
+            {
+            }
+        }
+
+        private void bunifuDropdown2_onItemSelected(object sender, EventArgs e)
+        {
+            ConnectToSQL con = new ConnectToSQL();
+            con.OpenConn();
+            string sql = "SELECT GIO FROM dbo.CHUYENBAY ORDER BY GIO ASC ";
+            SqlCommand cmd = new SqlCommand(sql, con.Connection);
+            SqlDataReader myReader;
+            try
+            {
+                myReader = cmd.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    while (myReader.Read())
+                    {
+                        bunifuDropdown2.AddItem(myReader[0].ToString());
+                    }
+                }
+            }
+            catch { }
+            con.CloseConn();
+        }
+
+        private void searchBt_Click(object sender, EventArgs e)
+        {
+            ConnectToSQL con = new ConnectToSQL();
+            con.OpenConn();
+            string sql = "SELECT MACHUYENBAY, SANBAYDI, SANBAYDEN, GIO, HANG1CONLAI, GIAVE*1.05 as price1, HANG2CONLAI, GIAVE " +
+                "FROM dbo.CHUYENBAY " +
+                "WHERE SANBAYDI='" + yearRp.selectedValue +
+                "' AND SANBAYDEN='" + bunifuDropdown1.selectedValue +
+                "' AND NGAY='" + bunifuDatepicker1.Value.ToString() + "'";
+            if(bunifuDropdown2.selectedValue!="Any Time")
+                sql+="AND GIO='" + bunifuDropdown2.selectedValue + "'";
+            SqlCommand cmd = new SqlCommand(sql, con.Connection);
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.CloseConn();
+            flightInfo.DataSource = dt;
         }
     }
 }
