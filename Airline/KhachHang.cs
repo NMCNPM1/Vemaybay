cafb10f8490ec51b9282d6297f396f55a672ba0c
nguyenbmt2418 <38326041+nguyenbmt2418@users.noreply.github.com>
2018-05-11 13:48:14 +0700

    Add files via upload

---
+++ Airline/KhachHang.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline.DTO
+{
+    class KhachHang
+    {
+        private string mAKHACHHANG;
+        private string hOTEN;
+        private bool gIOITINH;
+        private string cMND;
+        private string sDT;
+        private string eMAIL;
+        public KhachHang(string mkh, string hoten, bool gioitinh, string cmnd, string sdt, string email)
+        {
+            this.MAKHACHHANG = mkh;
+            this.HOTEN = hoten;
+            this.GIOITINH = gioitinh;
+            this.cMND = cmnd;
+            this.sDT = sdt;
+            this.eMAIL = email;
+        }
+        public KhachHang() { }
+
+        public string MAKHACHHANG { get => mAKHACHHANG; set => mAKHACHHANG = value; }
+        public string HOTEN { get => hOTEN; set => hOTEN = value; }
+        public bool GIOITINH { get => gIOITINH; set => gIOITINH = value; }
+        public string CMND { get => cMND; set => cMND = value; }
+        public string SDT { get => sDT; set => sDT = value; }
+        public string EMAIL { get => eMAIL; set => eMAIL = value; }
+    }
+}
---
+++ Airline/KhachHangMod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Airline.DTO;
+
+namespace Airline.Model
+{
+    class KhachHangMod
+    {
+        ConnectToSQL con = new ConnectToSQL();
+        SqlCommand cmd = new SqlCommand();
+        public bool AddData(KhachHang KH)
+        {
+
+            cmd.CommandText = " insert into KhachHang values ('" + KH.MAKHACHHANG + "','" + KH.HOTEN + "','" + KH.GIOITINH + "','" + KH.CMND + "','" + KH.SDT + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                con.CloseConn();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+    }
+}
